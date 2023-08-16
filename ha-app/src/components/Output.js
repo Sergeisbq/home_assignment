77b72f9a3de21debc8484f56275b439b997bb7d1
Sergeisbq <ss_b@icloud.com>
2023-08-16 14:10:03 +0300

    1

--- ha-app/src/components/Output.js
+++ ha-app/src/components/Output.js
@@ -1,19 +1,18 @@
-import { useContext } from 'react';
-import { AppContext } from '../App';
-import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import { AppContext } from "../App";
+import { Link } from "react-router-dom";
 
 const OutputRes = () => {
-
   const { text } = useContext(AppContext);
-  const lines = text.split('\n');
+  const lines = text.split("\n");
 
   return (
     <div>
-      <button><Link to="/read-file">Back</Link></button>
+      <button>
+        <Link to="/read-file">Back</Link>
+      </button>
       {lines.map((line, i) => (
-        <p key={i}>
-          {line}
-        </p>
+        <p key={i}>{line}</p>
       ))}
     </div>
   );
